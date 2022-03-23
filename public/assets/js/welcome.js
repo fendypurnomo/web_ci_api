6c570d7e85df65ba93a8046be21461b88da2925b
Fendy Purnomo <fendy.purnomo@yahoo.com>
2022-03-23 17:20:24 +0700

    upload

--- public/assets/js/welcome.js
+++ public/assets/js/welcome.js
@@ -1,7 +1,8 @@
 function toggleMenu() {
-	var menuItems = document.getElementsByClassName("menu-item");
-	for (var i = 0; i < menuItems.length; i++) {
-		var menuItem = menuItems[i];
-		menuItem.classList.toggle("hidden");
-	}
+  var menuItems = document.getElementsByClassName("menu-item");
+
+  for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.classList.toggle("hidden");
+  }
 }
--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ $paths = new Config\Paths();
 
 // Location of the framework bootstrap file.
 $bootstrap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$app       = require realpath($bootstrap) ?: $bootstrap;
+$app = require realpath($bootstrap) ?: $bootstrap;
 
 /*
  *---------------------------------------------------------------
